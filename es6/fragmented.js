2e8fe66113c1c4024323e28984bb0c8585674c57
James Smith <james.smith@djalbat.com>
2017-03-01 22:50:30 +0000

    Simplfied things still further.

--- es6/fragmented.js
+++ es6/fragmented.js
@@ -1,29 +1,17 @@
 'use strict';
 
 (function() {
-  const changeHandlers = [];
-
-  window.onhashchange = function() {
-    changeHandlers.forEach(function(changeHandler) {
-      changeHandler();
-    })
-  };
-
   function getFragment() {
     const hash = window.location.hash,
           fragment = hash.substr(1),  ///
           fragmentPrototype = Object.getPrototypeOf(fragment);
 
     fragmentPrototype.onChange = function(changeHandler) {
-       changeHandlers.push(changeHandler);
+       window.addEventListener('hashchange', changeHandler);
     };
 
     fragmentPrototype.offChange = function(changeHandler) {
-      const index = changeHandlers.indexOf(changeHandler);
-
-      if (index > -1) {
-        changeHandlers.splice(index, 1)
-      }
+      window.removeEventListener('hashchange', changeHandler);
     };
 
     return fragment;
