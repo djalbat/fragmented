8f51f5c990046a48615d1a5f5986c69ec73e1d56
James Smith <james.smith@djalbat.com>
2017-03-02 08:23:39 +0000

    Simplifed the implementation even more. Taken out uneccessary dependencies.

--- es6/fragmented.js
+++ es6/fragmented.js
@@ -1,13 +1,12 @@
 'use strict';
 
-(function() {
-  function getFragment() {
-    const hash = window.location.hash,
-          fragment = hash.substr(1),  ///
+Object.defineProperty(window, 'fragment', {
+  get: function () {
+    const fragment = window.location.hash.substr(1),  ///
           fragmentPrototype = Object.getPrototypeOf(fragment);
 
     fragmentPrototype.onChange = function(changeHandler) {
-       window.addEventListener('hashchange', changeHandler);
+      window.addEventListener('hashchange', changeHandler);
     };
 
     fragmentPrototype.offChange = function(changeHandler) {
@@ -15,16 +14,9 @@
     };
 
     return fragment;
+  },
+
+  set: function (fragment) {
+    window.location.hash = fragment;  ///
   }
-
-  function setFragment(fragment) {
-    const hash = fragment;  ///
-
-    window.location.hash = hash;
-  }
-
-  Object.defineProperty(window, 'fragment', {
-    get: getFragment,
-    set: setFragment
-  });
-})();
+});
